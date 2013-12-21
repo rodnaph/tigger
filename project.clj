6684cead3136ac88bf4bcb3b9e198e2d6e9715f3
Rhodri Pugh <rod@pu-gh.com>
2013-12-21 09:40:36 +0000

    bump version because of BC change

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
 
-(defproject rodnaph/tigger "0.2.0"
+(defproject rodnaph/tigger "0.3.0"
   :description "SMTP to core.async Channel Receiver"
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [org.clojure/core.async "0.1.242.0-44b1e3-alpha"]
